a2546669258883aa9e5043f0a2d1df7245be81c4
aogonowska <alicja.ogonowska@euvic.pl>
2018-08-12 10:49:59 +0200

    Updated message generator

--- mqtt/src/main/java/pl/polsl/tpdia/mqtt/MqttPublisher.java
+++ mqtt/src/main/java/pl/polsl/tpdia/mqtt/MqttPublisher.java
@@ -1,8 +1,12 @@
 package pl.polsl.tpdia.mqtt;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Random;
+
 import org.eclipse.paho.client.mqttv3.MqttClient;
 import org.eclipse.paho.client.mqttv3.MqttMessage;
-
+import org.json.JSONObject;
 
 public class MqttPublisher {
 	public static void main(String[] args) {
@@ -11,15 +15,20 @@ public class MqttPublisher {
 		try {
 			client = new MqttClient("tcp://test.mosquitto.org:1883", MqttClient.generateClientId());
 			client.connect();
-
+			Random generator = new Random();
 			while (true) {
+
 				MqttMessage message = new MqttMessage();
-				message.setPayload("Hello world from Java".getBytes());
+				JSONObject object = new JSONObject();
+				Date date = new Date();
+				SimpleDateFormat sfd = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+				object.put("date", sfd.format(date));
+				object.put("value", generator.nextDouble() * 100);
+				message.setPayload(object.toString().getBytes());
 				System.out.println("Publishing message!");
 				client.publish("tpdia_test", message);
 				Thread.sleep(1000);
 			}
-//			client.disconnect();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
